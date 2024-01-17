a5fed7a4f3b27dc9d76abb10a320f696ccf4571c
dodunlad3 <75912431+dodunlad3@users.noreply.github.com>
2024-01-17 13:19:13 -0500

    SplashTect and Header Component

---
+++ src/Components/HeaderDisplay/HeaderDisplay.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./HeaderDisplay.css";
+import SplashTxt from "../SplashText/SplashText.js";
+
+import placeHolder from "../../images/placeholder.png";
+
+function HeaderDisplay() {
+  return (
+    <div className="header-display-container">
+        <img src={placeHolder} />
+        <SplashTxt />
+    </div>
+  );
+}
---
+++ src/Components/SplashText/SplashTxt.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function SplashTxt() {
+  return (
+    <div>
+        <h3>SPLASH TEXT HERE</h3>
+    </div>
+  );
+}
