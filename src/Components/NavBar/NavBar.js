3ebd3e15cbf95b01df13d44717a7f3f94f50ef8f
dodunlad3 <75912431+dodunlad3@users.noreply.github.com>
2024-01-17 13:06:49 -0500

    Create NavBar Component

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,17 @@
+import react from 'react';
+import './NavBar.css';
+
+function NavBar() {
+    return (
+        <div>
+            <ul id="nav">
+                <li><a href="#">Home</a></li>  
+                <li><a href="#">About</a></li>
+                <li><a href="#">FAQ</a></li>
+            </ul>
+        </div>
+        
+    );
+}
+
+export default NavBar;
